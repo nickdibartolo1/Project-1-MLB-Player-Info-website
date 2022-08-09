12bac9424ffa4059ac5c41c5a2673029d382a523
bilalfarrukh2000 <bilalfarrukh2000@gmail.com>
2022-08-09 00:26:56 -0400

    Display content and results

--- assets/search_result.js
+++ assets/search_result.js
@@ -25,7 +25,36 @@ function getApi() {
 
 	var myApiUrl = "http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='" + playerName + "%25'";
 	console.log(playerName);
-	
+	$.get(myApiUrl,function(data){
+
+		console.log("success");
+		console.log(data);
+
+		$("#result-content").hide();
+		$("#result-content").empty();
+
+		var name=data.search_player_all.queryResults.row.name_first + " "+data.search_player_all.queryResults.row.name_last;
+		var birthCity=data.search_player_all.queryResults.row.birth_city;
+		var heightFt=data.search_player_all.queryResults.row.height_feet;
+		var heightInches=data.search_player_all.queryResults.row.height_inches;
+		var team=data.search_player_all.queryResults.row.team_full;
+		var position=data.search_player_all.queryResults.row.position;
+
+		$("#result-content").append(
+			"<h6>Name: "+name+"</h6>"+
+			"<h6>Birth City: "+birthCity+"</h6>"+
+			"<h6>Height: "+heightFt+" ft "+heightInches+" inches</h6>"+
+			"<h6>Team: "+team+"</h6>"+
+			"<h6>Position: "+position+"</h6>"
+
+		);
+
+		$("#result-content").show();
+
+	})
+
+}
+
 	
 
 
