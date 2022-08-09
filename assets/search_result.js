78e945ca67518d6a4cbcbe5657a7d451c37b0428
nickdibartolo1 <dibartolonick0518@gmail.com>
2022-08-08 21:45:30 -0400

    deletion of script.js file, commented out hitting/pitching api/filter

--- assets/search_result.js
+++ assets/search_result.js
@@ -24,14 +24,7 @@ function getApi() {
 
 	var myApiUrl = "http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='" + playerName + "%25'"
 	console.log(playerName)
-	//var formatName = document.getElementById("format-input");
-	// if (format) {
-	// 	myApiUrl = "http://lookup-service-prod.mlb.com/json/named." + format + ".bam?sport_code='mlb'&active_sw='Y'";
-	// }
-
-	// myApiUrl = myApiUrl + "&name_part='" + playerName + "%25'" + query;
-
-
+	
 	fetch(myApiUrl)
 		.then(function (response) {
 			return response.json();
@@ -64,34 +57,34 @@ function getApi() {
 }
 
 
-function hittingApi() {
-	var hittingUrl = "http://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id='mlb'&game_type='R'&player_id='493316'"
+// function hittingApi() {
+// 	var hittingUrl = "http://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id='mlb'&game_type='R'&player_id='493316'"
 
 
-	fetch(hittingUrl)
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (data) {
-			console.log(data.sport_career_hitting.queryResults)
+// 	fetch(hittingUrl)
+// 		.then(function (response) {
+// 			return response.json();
+// 		})
+// 		.then(function (data) {
+// 			console.log(data.sport_career_hitting.queryResults)
 
-		})
+// 		})
 
-}
-// hittingApi()
+// }
+// // hittingApi()
 
-function pitchingApi() {
-	var pitchingUrl = "http://lookup-service-prod.mlb.com/json/named.sport_career_pitching.bam?league_list_id='mlb'&game_type='R'&player_id='592789'"
+// function pitchingApi() {
+// 	var pitchingUrl = "http://lookup-service-prod.mlb.com/json/named.sport_career_pitching.bam?league_list_id='mlb'&game_type='R'&player_id='592789'"
 
-	fetch(pitchingUrl)
-		.then(function (response) {
-			return response.json();
-		})
-		.then(function (data) {
-			console.log(data.sport_career_pitching.queryResults)
+// 	fetch(pitchingUrl)
+// 		.then(function (response) {
+// 			return response.json();
+// 		})
+// 		.then(function (data) {
+// 			console.log(data.sport_career_pitching.queryResults)
 
-		})
-}
+// 		})
+// }
 // pitchingApi()
 
 function printResults(resultObj) {
